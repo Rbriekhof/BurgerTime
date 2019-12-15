ab865ab3b359231267faf062bce094d071b4e7d2
Robert Riekhof <RobertBRiekhof@gmail.com>
2019-12-15 17:45:46 -0600

    Main Updates

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+### Populate data with 3 starter burgers
+INSERT INTO burgers (burger_name) VALUES ('Double Bacon Cheeseburger');
+
+INSERT INTO burgers (burger_name) VALUES ('Royale with Cheese');
+
+INSERT INTO burgers (burger_name) VALUES ('QuarterPounder with Cheese');
